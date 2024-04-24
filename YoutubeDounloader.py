c071cf0f492277825a7ed69bd66126b3f7da4bad
Jot Waraich <gurjagjot@gmail.com>
2024-04-23 22:51:52 -0600

    first commit

---
+++ YoutubeDounloader.py
@@ -0,0 +1,14 @@
+from pytube import YouTube
+from sys import argv
+
+# link = input("Enter link : ")
+link = argv[1]
+yt = YouTube(link)
+
+print("tital :", yt.title)
+print("views :", yt.views)
+
+yd = yt.streams.get_highest_resolution() #for the highest resolution
+# yd = yt.streams.get_by_resolution(1080)
+path = "S:\\learning\\python automation\\youtube downloader\\videos"
+yd.download(path)
---
+++ yt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd "S:\learning\python automation\youtube downloader"
+
+echo "Enter the link : "
+read my_var
+
+python "S:\learning\python automation\youtube downloader\YoutubeDounloader.py" "${my_var}"
